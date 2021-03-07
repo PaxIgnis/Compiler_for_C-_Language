7256d53f50c3a88b6e0faa5feb8f1ae76268690e
Meijden <Hvanderm@uoguelph.ca>
2021-03-07 17:25:33 -0500

    Added visitor tree traversal code

--- ShowTreeVisitor.java
+++ ShowTreeVisitor.java
@@ -15,6 +15,20 @@ public class ShowTreeVisitor implements AbsynVisitor {
     } 
   }
 
+  public void visit( DecList decList, int level ) {
+    while( decList != null ) {
+      decList.head.accept( this, level );
+      decList = decList.tail;
+    } 
+  }
+
+  public void visit( VarDecList varDecList, int level ) {
+    while( varDecList != null ) {
+      varDecList.head.accept( this, level );
+      varDecList = varDecList.tail;
+    } 
+  }
+
   public void visit( AssignExp exp, int level ) {
     indent( level );
     System.out.println( "AssignExp:" );
@@ -63,6 +77,18 @@ public class ShowTreeVisitor implements AbsynVisitor {
       case OpExp.GT:
         System.out.println( " > " );
         break;
+      case OpExp.GTEQ:
+        System.out.println( " >= " );
+        break;
+      case OpExp.LTEQ:
+        System.out.println( " <= " );
+        break;
+      case OpExp.EQUALS:
+        System.out.println( " == " );
+        break;
+      case OpExp.NEQUALS:
+        System.out.println( " != " );
+        break;
       default:
         System.out.println( "Unrecognized operator at line " + exp.row + " and column " + exp.col);
     }
@@ -71,29 +97,92 @@ public class ShowTreeVisitor implements AbsynVisitor {
     exp.right.accept( this, level );
   }
 
-  public void visit( ReadExp exp, int level ) {
-    indent( level );
-    System.out.println( "ReadExp:" );
-    exp.input.accept( this, ++level );
-  }
-
-  public void visit( RepeatExp exp, int level ) {
-    indent( level );
-    System.out.println( "RepeatExp:" );
-    level++;
-    exp.exps.accept( this, level );
-    exp.test.accept( this, level ); 
-  }
-
   public void visit( VarExp exp, int level ) {
     indent( level );
-    System.out.println( "VarExp: " + exp.name );
+    System.out.println( "VarExp: " );
+    exp.variable.accept(this, ++level );
   }
 
-  public void visit( WriteExp exp, int level ) {
+  public void visit( NilExp exp, int level ) {
     indent( level );
-    System.out.println( "WriteExp:" );
-    exp.output.accept( this, ++level );
+    System.out.println( "NilExp");
+  }
+
+  public void visit( NameTy typ, int level ) {
+    indent( level );
+    System.out.print( "NameTy:" ); 
+    switch( typ.typ ) {
+      case NameTy.INT:
+        System.out.println( " INT " );
+        break;
+      case NameTy.VOID:
+        System.out.println( " VOID " );
+        break;
+      default:
+        System.out.println( "Unrecognized type at line " + typ.row + " and column " + typ.col);
+    }
+  }
+
+  public void visit( SimpleVar var, int level ) {
+    indent( level );
+    System.out.println( "SimpleVar: " + var.name );
+  }
+
+  public void visit( IndexVar var, int level ) {
+    indent( level );
+    System.out.println( "IndexVar: " + var.name );
+    var.index.accept(this, ++level );
+  }
+
+  public void visit( CallExp exp, int level ) {
+    indent( level );
+    System.out.println( "CallExp: " + exp.func );
+    exp.args.accept(this, ++level );
+  }
+
+  public void visit( WhileExp exp, int level ) {
+    indent( level );
+    System.out.println( "WhileExp:" );
+    level++;
+    exp.body.accept(this, level );
+    exp.test.accept(this, level );
+  }
+
+  public void visit( ReturnExp exp, int level ) {
+    indent( level );
+    System.out.println( "ReturnExp:" );
+    exp.exp.accept(this, ++level );
+  }
+
+  public void visit( CompoundExp exp, int level ) {
+    indent( level );
+    System.out.println( "CompoundExp: " );
+    level++;
+    exp.decs.accept(this, level );
+    exp.exps.accept(this, level );
+  }
+
+  public void visit( FunctionDec dec, int level ) {
+    indent( level );
+    System.out.println( "FunctionDec: " + dec.func );
+    level++;
+    dec.result.accept(this, level );
+    dec.params.accept(this, level );
+    dec.body.accept(this, level );
+  }
+
+  public void visit( SimpleDec varDec, int level ) {
+    indent( level );
+    System.out.println( "SimpleDec: " + varDec.name );
+    varDec.typ.accept(this, ++level );
+  }
+
+  public void visit( ArrayDec varDec, int level ) {
+    indent( level );
+    System.out.println( "ArrayDec: " + varDec.name );
+    level++;
+    varDec.typ.accept(this, level );
+    varDec.size.accept(this, level );
   }
 
 }
